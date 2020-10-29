3d6c86e3c3f327e2c48a98cf82163da30cfb57ed
yujin <i5533m@naver.com>
2020-10-29 22:48:53 +0900

    20201029

--- pythoncode/opencsv.py
+++ pythoncode/opencsv.py
@@ -1,6 +1,8 @@
 import csv
 import io
 
+#사실 utf-8이랑 utf8이랑 둘다 같다. 참고로 여기서 io.open 안하면 encoding이 제대로 안됨ㅠㅠ
+#I think utf-8 and utf8 are just same.. and if I don't put "io" infront of open function, it cause error.
 def opencsv(filename):
     f=io.open(filename,'r' ,encoding='utf-8')
     rdr=csv.reader(f)
